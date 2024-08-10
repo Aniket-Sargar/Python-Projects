dce65b28e174a7ae3addd867a4b30a8ab87954d4
Aniket Sargar <aniketsargar93@gmail.com>
2024-08-10 20:15:32 +0530

    Delete .ipynb_checkpoints directory

--- .ipynb_checkpoints/passward.py
+++
@@ -1,9 +0,0 @@
-username = input("Enter username: ")
-password = input("Enter password: ")
-username == "Aniket"
-password = "AS2005"
-if(username == "Aniket" and password = "AS2005"):
-    print("Welcome"+" "+username)
-else:
-    print("please check username or password")    
-
